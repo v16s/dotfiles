514703443cdec16ee7a94fc359ce11b2826dedff
v16s <shankar.vignesh@gmail.com>
2019-09-14 12:58:53 +0530

    initial version

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+set -gx BROWSER firefox
+set -gx ANDROID_HOME /opt/android-sdk
+set -gx PATH $PATH /opt/android-sdk/platform-tools/
+set fish_greeting
+alias scr "scrot -sb ~/Pictures/Screenshots"
---
+++ .config/polybar/init.sh
@@ -0,0 +1,17 @@
+#!/bin/env sh
+
+pkill polybar
+
+sleep 1;
+
+# polybar i3wmthemer_bar &
+
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload i3wmthemer_bar &
+#   done
+# else
+#   polybar --reload i3wmthemer_bar &
+# fi
+polybar --reload dp &
+polybar --reload hdmi &
